3cfafd2a6df23925ca813e9b08997055fa8013e4
Mango-Haprcom <107311727+UKMango@users.noreply.github.com>
2023-07-18 09:28:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,24 @@
 var config = {
     texts: [
-        "靓仔爸爸，今天是你的生日",      
-        "靓仔生日快乐！",   
-        "祝父亲生意兴隆",
-        "福如东海",
-        "寿比南山",
+        "哥，今天是你的生日",      
+        "生日快乐！",   
+        "祝你工作顺心",
+        "事事如意",
+        "健康平安",
+        "心想事成",
         ],
     imgs: {
-        "送花！": "./imgs/map.gif",
+        "送花": "./imgs/map.gif",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开灯～",
+        turn_on: "开下灯",
         // play: "音乐",
-        bannar_coming: "Surprise惊喜！",
+        bannar_coming: "点趴",
         balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕！",
-        light_candle: "点蜡烛",
+        cake_fadein: "蛋糕",
+        light_candle: "点亮蜡烛",
         wish_message: "生日快乐！",
-        story: "靓仔Happy Birthday！",
+        story: "Happy Birthday！",
     }
 };
